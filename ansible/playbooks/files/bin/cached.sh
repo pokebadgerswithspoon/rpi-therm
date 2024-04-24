96588425233ad440d5733dac492a16ce000c4348
Lauri Saks <lauri@icantfeelmylegs.com>
2024-04-24 11:49:14 +0300

    cache

---
+++ ansible/playbooks/files/bin/cached.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+CMD=$*
+TMPDIR=${TMPDIR:-"/tmp"}
+CACHE_NAME=${CACHE_NAME:-cached}
+CACHE_FILE="${TMPDIR}/${CACHE_NAME}"
+CACHE_TTL=${CACHE_TTL:-1}
+
+out() {
+  MSG=$*
+  printf "%s" "$MSG" > "$CACHE_FILE"
+  printf "%s" "$MSG"
+}
+
+if [ -f "$CACHE_FILE" ]; then
+   CACHE_AGE=$(($(date +%s) - $( stat -c "%Y" "${CACHE_FILE}")))
+   if [ "$CACHE_AGE" -le "${CACHE_TTL}" ]; then
+      cat "${CACHE_FILE}"
+      exit
+   fi
+fi
+
+out "$(sh -c "${CMD}")"
