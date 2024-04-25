ea010a833890196deb5b0d1c2d75ebf4cd10a51b
Lauri Saks <lauri@icantfeelmylegs.com>
2024-04-25 19:48:05 +0300

    adjust post.sh, so it would work with q

--- ansible/playbooks/files/bin/thermometer_post.sh
+++ ansible/playbooks/files/bin/thermometer_post.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-
-BIN_DIR=$(dirname "$0")
-
 die() {
   printf "ERROR: %s" "$1"
   exit 1
 }
+
 [ -x "$(command -v curl)" ] || die "curl is not present"
+
+BIN_DIR=$(dirname "$0")
 
 JWT=${JWT:-$("${BIN_DIR}/cached_jwt.sh")}
 URL=${URL:-"https://raimond.icantfeelmylegs.com/thermometer"}
 JSON=$("${BIN_DIR}/cached_json.sh")
 
-curl -X POST \
- -d \'"${JSON}"\' \
- -H 'Accept: application/json' \
- -H "Authorization: Bearer ${JWT}" \
- "${URL}"
+CMD="curl -X POST \
+ -d '${JSON}' \
+ -H \"Authorization: Bearer ${JWT}\" \
+ ${URL}"
+sh -c "${CMD}"
